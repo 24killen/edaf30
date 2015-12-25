9e57285914904bd2a2d4f66acdaea709fdabc960
ArturHellberg <artur.hellberg@gmail.com>
2015-12-25 16:32:24 +0100

    Update main.cpp

--- Labb4/Labb4U3/main.cpp
+++ Labb4/Labb4U3/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <vector>
+#include "Bostad.h"
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main(){
+    vector<int> sovrumsstorlek{16,14};
+    vector<int> garderober{4,3};
+    Bostad minLya(12,false,true,    //Kök
+                   4,true,          //Badrum
+                   24,false,        //vardagsrum
+                   sovrumsstorlek,
+                   garderober);
+    minLya.skriv();
     return 0;
 }
