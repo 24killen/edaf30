c961e3f8bc5008420259e8f82fc39cd5abab424e
ArturHellberg <artur.hellberg@gmail.com>
2015-12-25 16:23:12 +0100

    Update main.cpp

--- Labb4/Labb4U2/main.cpp
+++ Labb4/Labb4U2/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include "Pet.h"
+#include "Cat.h"
+#include "Dog.h"
+#include "Bird.h"
+#include <vector>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main(){
+
+    Pet* pointer;
+    Bird b = Bird("Ho Ho");
+    Cat c = Cat("Mjau");
+    Dog d = Dog("Woof");
+    std::vector<Pet*> v;
+
+    v.push_back(&b);
+    v.push_back(&c);
+    v.push_back(&d);
+
+    for(int i = 0; i<v.size();++i){
+        pointer = v.at(i);
+        cout << (pointer->speak()) << endl;
+   }
     return 0;
 }
