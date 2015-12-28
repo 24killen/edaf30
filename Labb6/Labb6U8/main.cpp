ee03d0dade4a6a39112527664f7a2bcad7d1fa5e
ArturHellberg <artur.hellberg@gmail.com>
2015-12-28 21:58:21 +0100

    Labb6U8
    Labb6U8 avklarad.

--- Labb6/Labb6U8/main.cpp
+++ Labb6/Labb6U8/main.cpp
@@ -10,34 +10,34 @@ union Data{
 
 int main(){
     stringstream ss1, ss2, ss3, ss4;        //hex-converter
-    int theNbr = 12345678;    //the hex if 0x0fedc, 65244
+    int theNbr = 305419896;    //the hex is 12345678
     Data memory;
     string result;
     ss1<<hex<<theNbr;
-    cout<<"TheNbr: "<<ss1.str()<<endl;
+    cout<<"Original: "<<ss1.str()<<endl;
 
     memory.four_bytes = theNbr;
     ss2<<hex<<memory.four_bytes;
     cout<<"Result 1: "<<ss2.str()<<endl;
 
 
-    memory.twoBytes[0] = theNbr>>12;
-    memory.twoBytes[1] = theNbr&0x0fff;
-    ss3<<hex<<memory.twoBytes[0];
+    memory.twoBytes[0] = theNbr&0x0ffff;
+    memory.twoBytes[1] = theNbr>>16;
+    ss3<<hex<<memory.twoBytes[0]<<" ";
     ss3<<hex<<memory.twoBytes[1];
     cout<<"Result 2: "<<ss3.str()<<endl;
 
 
+    memory.oneByte[0] = theNbr&0x0ff;
+    memory.oneByte[1] = theNbr>>8;
+    memory.oneByte[2] = theNbr>>16;
+    memory.oneByte[3] = theNbr>>24;
+    ss4<<hex<<(int)memory.oneByte[0]<<" ";   //12(hex) = <->(ASCII)
+    ss4<<hex<<(int)memory.oneByte[1]<<" ";   //34(hex) = 4(ASCII)
+    ss4<<hex<<(int)memory.oneByte[2]<<" ";   //56(hex) = V(ASCII)
+    ss4<<hex<<(int)memory.oneByte[3]<<" ";   //78(hex) = x(ASCII)
+    cout<<"Result 3: "<<ss4.str()<<endl;
 
-
-//    memory.oneByte[0] = theNbr & (0xf000>>i);
-//        cout<<memory.oneByte[i]<<endl;
-
-    int sum = 0;
-    for(int i=0;i<4;++i){
-        sum += memory.oneByte[i];
-    }
-//    cout<<sum;
     return 0;
 }
 
