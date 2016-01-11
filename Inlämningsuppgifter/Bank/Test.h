36d0682ce0463a443c65ef1768ba0569d077aaa9
24killen <24killen@gmail.com>
2016-01-11 15:51:15 +0100

    Redesign and created test on Bank

---
+++ Inlämningsuppgifter/Bank/Test.h
@@ -0,0 +1,15 @@
+#ifndef TEST_H
+#define TEST_H
+
+
+class Test
+{
+    public:
+        Test();
+        virtual ~Test();
+        void startaTest();
+    protected:
+    private:
+};
+
+#endif // TEST_H
